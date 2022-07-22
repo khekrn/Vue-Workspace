e4afd07ea9db85f290438d995e1b0a5892d18535
khekrn <kishorenayark@gmail.com>
2022-07-22 08:56:30 +0530

    Initial Commit Vue 3

---
+++ app-2/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import TagsInput from "./components/TagsInput.vue";
+export default {
+  components: {
+    TagsInput
+    //TagsInput,
+  },
+};
+</script>
+
+<template>
+  <h1>Hello World</h1>
+  <tags-input></tags-input>
+  <tags-input />
+</template>
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+h1 {
+  color: blue;
+}
+</style>
